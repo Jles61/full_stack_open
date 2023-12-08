ba1b695e145b8660ec30a95605d3a9e0d3ead360
jules <jules.hrt91@gmail.com>
2023-12-08 23:17:51 +0100

    fix repo

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Course = ({ course }) => {
+  return (
+    <div>
+        <h2>{course.name}</h2>
+        <div>{course.parts.map((part) => <p key={part.id}>{part.name} {part.exercises}</p>)}</div>
+        <h3>total of {course.parts.reduce((sum, part) => sum+part.exercises, 0)} exercises</h3>
+    </div>
+  )
+}
+
+export default Course

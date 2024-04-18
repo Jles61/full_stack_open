6ad8575bed5ad9d0fb5b53b33dbc3c30482cf412
jhenriette <jules.henriette@vistory.com>
2024-04-18 10:40:33 +0200

    remove cors part3-b

--- part3/part3-notes-backend/index.js
+++ part3/part3-notes-backend/index.js
@@ -29,9 +29,9 @@ const requestLogger = (request, response, next) => {
     next()
 }
 
-const cors = require('cors')
+// const cors = require('cors')
 
-app.use(cors())
+// app.use(cors())
 
 app.use(express.json())
 app.use(requestLogger)
